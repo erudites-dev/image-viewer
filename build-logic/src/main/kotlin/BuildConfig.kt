cbf8fb5b12cb785f1f74122ba7b2742d13c0b709
Seungyeop Lee <lsy020206@outlook.kr>
2026-06-26 16:26:57 +0900

    update fabric, neoforge and mcef

--- build-logic/src/main/kotlin/BuildConfig.kt
+++ build-logic/src/main/kotlin/BuildConfig.kt
@@ -6,12 +6,12 @@ object BuildConfig {
     val MINECRAFT_VERSION_RANGE: String = ">=26.2" // range: ">=26.1 <27.1"
     val MINECRAFT_VERSION_MIN: String = MINECRAFT_VERSION_RANGE.split(" ")[0].replace(Regex("^[><=!\\[\\]()]+"), "")
     val MINECRAFT_VERSION: String = "26.2"
-    val NEOFORGE_VERSION: String = "26.2.0.0-beta"
-    val FABRIC_LOADER_VERSION: String = "0.19.2"
-    val FABRIC_API_VERSION: String = "0.152.1+26.2"
+    val NEOFORGE_VERSION: String = "26.2.0.7-beta"
+    val FABRIC_LOADER_VERSION: String = "0.19.3"
+    val FABRIC_API_VERSION: String = "0.153.0+26.2"
     val PAPER_VERSION: String = "26.2.build.+"
 
-    val MCEF_VERSION: String = "2.2.0-26.1.1"
+    val MCEF_VERSION: String = "2.2.1-26.2"
 
     // https://semver.org/
     var MOD_VERSION: String = "0.1.0"
