71bd75c0e29418fb89348fdc3f468724bda84554
Seungyeop Lee <lsy020206@outlook.kr>
2026-09-16 20:19:18 +0900

    fix run paper

--- paper/build.gradle.kts
+++ paper/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("multiloader-platform")
     id("java-library")
     id("io.papermc.paperweight.userdev") version("2.0.0-beta.21")
-    id("xyz.jpenilla.run-paper") version("3.0.2")
+    id("xyz.jpenilla.run-paper") version("3.1.0")
 }
 
 base {
